3ea2ee53c15dfd4817354c9c6806ac323de1094a
wol pumba <wolpumba@gmail.com>
2026-08-08 05:46:18 +0000

    fix(build): add default rel and configure NASM flags to prevent reloc-rel-dword errors in CMake

--- fftasm64.s
+++ fftasm64.s
@@ -20,6 +20,7 @@
 ; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
 ; OR OTHER DEALINGS IN THE SOFTWARE.
 section .note.GNU-stack
+default rel
 
 extern lir_sched_yield
 global asmbulk_of_dual_dif
